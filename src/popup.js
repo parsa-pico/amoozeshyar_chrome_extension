3d4fbcf53d79019e8742c61c9f8c872003e33ffa
parsa-pico <parsaforoozmand@gmail.com>
2025-02-22 16:46:06 +0330

    #

--- src/popup.js
+++ src/popup.js
@@ -1,14 +1,5 @@
-// document.getElementById("alertButton").addEventListener("click", function () {
-//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//         if (tabs.length === 0) return;
-
-//         // Inject the content script dynamically
-//         chrome.scripting.executeScript({
-//             target: { tabId: tabs[0].id },
-//             files: ["content-script.js"]
-//         }, () => {
-//             // Send a message to the content script after injection
-//             chrome.tabs.sendMessage(tabs[0].id, { action: "showAlert" });
-//         });
-//     });
-// });
+document.getElementById('alertButton').addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    alert('helllo');
+  });
+});
